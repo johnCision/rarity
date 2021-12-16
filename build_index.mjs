843530a24b44506593199de9875e01613278e10c
John Talton <john.talton@cision.com>
2021-12-16 14:23:44 -0500

    move include to index

--- build_index.mjs
+++ build_index.mjs
@@ -1,4 +1,5 @@
 import * as fs from 'fs/promises'
+import { readFileSync } from 'fs'
 
 import Handlebars from 'handlebars'
 
@@ -6,7 +7,7 @@ const UTF_8 = 'utf8'
 
 async function build_index(indexTemplate, options) {
 	Handlebars.registerHelper('include', (file, context, opt) => {
-		return file
+		return readFileSync(file, { encoding: UTF_8 })
 	})
 
 	const template = Handlebars.compile(indexTemplate)
@@ -17,14 +18,7 @@ async function build_index(indexTemplate, options) {
 const index_template = './public/index.handlebars'
 
 const applejacks_templates = (await Promise.all([
-	'./node_modules/@johncision/applejacks/templates/application-bar.html',
-	'./node_modules/@johncision/applejacks/templates/application-frame.html',
-	'./node_modules/@johncision/applejacks/templates/button-set.html',
-	'./node_modules/@johncision/applejacks/templates/button.html',
-	'./node_modules/@johncision/applejacks/templates/face.html',
-	'./node_modules/@johncision/applejacks/templates/icon.html',
-	'./node_modules/@johncision/applejacks/templates/progress.html',
-	'./node_modules/@johncision/applejacks/templates/text-input.html'
+
 ].map(filename => fs.readFile(filename, { encoding: UTF_8 }))))
 .join('\n')
 
